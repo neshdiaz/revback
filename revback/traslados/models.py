63b6ca3e6c683f66c510b87a670888bfe3019b6e
neshdiaz <nesh.diaz@gmail.com>
2024-08-02 17:20:37 -0500

    Ajustes en modelo de traslados

--- revback/traslados/models.py
+++ revback/traslados/models.py
@@ -1,14 +1,33 @@
 from django.db import models
+from usuarios.models import Usuario
+from plataformas.models import Plataforma
+from bodegas.models import Bodega
 
 
 class Traslado(models.Model):
     fecha = models.DateField()
-    notas = models.TextField()
+    notas = models.TextField(blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     # Foreign keys
-    responsable = models.CharField(max_length=128)
-    plataforma = models.CharField(max_length=128)
-    bodega_origen = models.CharField(max_length=128)
-    bodega_destino = models.CharField(max_length=128)
+    responsable = models.ForeignKey(
+        Usuario,
+        on_delete=models.PROTECT,
+        related_name="responsable_de_traslados",
+    )
+    plataforma = models.ForeignKey(
+        Plataforma,
+        on_delete=models.PROTECT,
+        related_name="traslados_realizados",
+    )
+    bodega_origen = models.ForeignKey(
+        Bodega,
+        on_delete=models.PROTECT,
+        related_name="traslados_desde",
+    )
+    bodega_destino = models.ForeignKey(
+        Bodega,
+        on_delete=models.PROTECT,
+        related_name="traslados_hacia",
+    )
