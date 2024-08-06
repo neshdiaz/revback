2b5ac29877fdec68ce6d09f9d75d6b1e81ed3cf8
neshdiaz <nesh.diaz@gmail.com>
2024-08-06 15:54:28 -0500

    Creados admins para cliente y ventas

--- revback/clientes/admin.py
+++ revback/clientes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from clientes.models import Cliente
+
+class ClienteAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Cliente, ClienteAdmin)
--- revback/ventas/admin.py
+++ revback/ventas/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from ventas.models import Venta
 
-# Register your models here.
+
+class VentaAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Venta, VentaAdmin)
