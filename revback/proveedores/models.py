ba3f38975348a70813d665c6e2f7ce8114b3a0e6
neshdiaz <nesh.diaz@gmail.com>
2024-08-28 10:59:45 -0500

    Campo email de proveedor se deja como campo opcional

--- revback/proveedores/models.py
+++ revback/proveedores/models.py
@@ -5,7 +5,7 @@ from usuarios.models import Usuario
 class Proveedor(models.Model):
     nombres = models.CharField(max_length=128)
     apellidos = models.CharField(max_length=128)
-    email = models.CharField(max_length=128, unique=True)
+    email = models.CharField(max_length=128, unique=True, null=True, blank=True)
     web = models.CharField(max_length=256, null=True, blank=True)
     whatsapp = models.CharField(max_length=128, null=True, blank=True)
     url_imagen = models.CharField(max_length=256, null=True, blank=True)
